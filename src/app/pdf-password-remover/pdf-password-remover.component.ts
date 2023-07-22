72d97d94139999078910979df42df5565681a96e
Sai Krishna Annam <sai.annam@betteranalytics.in>
2023-07-22 17:31:54 +0530

    feature: added the feature of drop here

--- src/app/pdf-password-remover/pdf-password-remover.component.ts
+++ src/app/pdf-password-remover/pdf-password-remover.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pdf-password-remover',
   templateUrl: './pdf-password-remover.component.html',
-  styleUrls: ['./pdf-password-remover.component.scss']
+  styleUrls: ['./pdf-password-remover.component.scss'],
 })
 export class PdfPasswordRemoverComponent {
   selectedFileNames: string[] = [];
@@ -26,6 +26,32 @@ export class PdfPasswordRemoverComponent {
     if (selectedFiles) {
       for (let i = 0; i < selectedFiles.length; i++) {
         const file = selectedFiles[i];
+        const fileExtension = file.name.split('.').pop()?.toLowerCase();
+        if (fileExtension === 'pdf') {
+          if (this.selectedFileNames.length < allowedFileCount) {
+            this.selectedFileNames.push(file.name);
+          } else {
+            alert('You can select a maximum of 5 PDF files.');
+            break;
+          }
+        } else {
+          alert('Please select a valid PDF file.');
+        }
+      }
+    }
+  }
+
+  onDragOver(event: Event) {
+    event.preventDefault();
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    const files = event.dataTransfer?.files;
+    const allowedFileCount = 5;
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
         const fileExtension = file.name.split('.').pop()?.toLowerCase();
         if (fileExtension === 'pdf') {
           if (this.selectedFileNames.length < allowedFileCount) {
