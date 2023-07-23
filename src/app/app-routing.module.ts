3ec42449253871db362bddb721bc82a13871d485
Sai  Annam <107242846+otaku0304@users.noreply.github.com>
2023-07-23 12:45:12 +0530

    feature: created new two components and adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { FeatureRoutingModule } from "./feature/feature-routing.module";
 const routes: Routes = [
     {
-        path:'',
+        path:'pdf',
         loadChildren: () =>
         import('./pdf-password-remover/pdf.module').then((m) => m.PDFModule),
     },
@@ -11,7 +11,17 @@ const routes: Routes = [
         path: '',
         loadChildren: () =>
             import('./feature/feature.module').then((m) => m.FeatureModule),
-    }
+    }, 
+    {
+        path: 'i18n',
+        loadChildren: () =>
+            import('./i18n/i18n.module').then((m) => m.I18nModule),
+    },
+    {
+        path: 'image',
+        loadChildren: () =>
+            import('./image-compressor/image.module').then((m) => m.ImageModule),
+    },
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes), FeatureRoutingModule],
