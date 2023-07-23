835e729e4e593410e6f06219b1f35b40b5ae0be9
Sai  Annam <107242846+otaku0304@users.noreply.github.com>
2023-07-23 11:27:30 +0530

    feature: successfully implemented landing page

--- src/app/pdf-password-remover/pdf.module.ts
+++ src/app/pdf-password-remover/pdf.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SelectPdfFilesComponent } from './select-pdf-files/select-pdf-files.component';
 import { PDFRoutingModule } from './pdf-routing.module';
+import { CommonModule } from '@angular/common';
 @NgModule({
-    declarations: [
-        SelectPdfFilesComponent
-    ], 
-    exports: [],
-    providers: [],
-    imports: [PDFRoutingModule]
+  declarations: [SelectPdfFilesComponent],
+  exports: [],
+  providers: [],
+  imports: [CommonModule, PDFRoutingModule],
 })
-export class PDFModule{}
+export class PDFModule {}
