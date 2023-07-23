568c256679a1df7109cd2dc6424fd9358b71b1d9
Sai  Annam <107242846+otaku0304@users.noreply.github.com>
2023-07-23 16:53:51 +0530

    feature: added a new component for asking the password

--- src/app/pdf-password-remover/pdf-routing.module.ts
+++ src/app/pdf-password-remover/pdf-routing.module.ts
@@ -1,11 +1,16 @@
 import { RouterModule, Routes } from '@angular/router';
 import { SelectPdfFilesComponent } from './select-pdf-files/select-pdf-files.component';
 import { NgModule } from '@angular/core';
+import { RemovePasswordComponent } from './remove-password/remove-password.component';
 
 export const routes: Routes = [
   {
     path: 'select-pdf-files',
     component: SelectPdfFilesComponent,
+  },
+  {
+    path: 'remove-password',
+    component: RemovePasswordComponent,
   },
 ];
 @NgModule({
