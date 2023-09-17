69cd63d52a2c58b783f76d703846c8b714f9b0b2
Sai Krishna Annam <sai.annam@betteranalytics.in>
2023-09-17 15:29:02 +0530

    added workflow

--- src/app/pdf-password-remover/download/download.component.ts
+++ src/app/pdf-password-remover/download/download.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-download',
   templateUrl: './download.component.html',
-  styleUrls: ['./download.component.scss']
+  styleUrls: ['./download.component.scss'],
 })
-export class DownloadComponent {
-
-}
+export class DownloadComponent {}
