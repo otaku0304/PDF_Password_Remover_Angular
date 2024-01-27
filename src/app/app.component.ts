42ac0dcde6f8ee9a154d790ff59ab8354d383444
SaiKrishnaAnnam <sai.annam@betteranalytics.in>
2024-01-27 17:13:59 +0530

    added sperate folder for footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'pdf-removal';
-  currentYear: number = new Date().getFullYear();
-}
+export class AppComponent {}
