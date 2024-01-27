13bdcc5e7b080ca9ad2c13fa7078e411967c3fad
SaiKrishnaAnnam <sai.annam@betteranalytics.in>
2024-01-27 16:11:14 +0530

    corrected the flask prod link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   development: false,
   local: false,
-  apiUrl: 'https://angular-pdf-pr-master.onrender.com/',
+  apiUrl: 'https://pdf-password-remover.onrender.com/',
 };
