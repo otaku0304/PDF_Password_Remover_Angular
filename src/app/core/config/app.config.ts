57e21d18e137fca3dfe374dd292fe54e19c691e1
SaiKrishnaAnnam <sai.annam@betteranalytics.in>
2024-01-27 15:57:21 +0530

    configured master dev local paths

--- src/app/core/config/app.config.ts
+++ src/app/core/config/app.config.ts
@@ -1,7 +1,9 @@
 import { environment } from './../../../environments/environment';
 
+
 export class AppConfig {
   public static getAPIURI(): string {
     return environment.apiUrl;
   }
+  
 }
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  development: true,
+  local: false,
+  apiUrl: 'https://pdf-password-remover-dev.onrender.com/',
+};
