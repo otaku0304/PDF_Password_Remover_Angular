01efed0474372fbafc519f3b60538d4ba4038adf
Sai Krishna Annam <sai.annam@rushcabs.in>
2024-06-01 12:06:33 +0530

    updated the flask dev link

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   development: true,
   local: false,
-  apiUrl: 'https://flask-pdf-pr-dev.onrender.com/',
+  apiUrl: 'https://flask-pdf-pr-main.onrender.com/',
 };
