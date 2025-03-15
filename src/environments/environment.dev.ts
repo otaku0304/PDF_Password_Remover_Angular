e22985cc32ce0f09a7b146dcb2bcd0249fd48785
Sai Annam <107242846+otaku0304@users.noreply.github.com>
2025-03-15 22:01:48 +0530

    fix: update apiUrl in environment.dev.ts for development environment

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   development: true,
   local: false,
-  apiUrl: (window as any).API_URL || 'http://127.0.0.1:5000/',
+  apiUrl: 'https://dev-pdf-flask.onrender.com',
 };
