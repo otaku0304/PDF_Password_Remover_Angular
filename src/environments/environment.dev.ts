9c5eefd8e29cf25ff68dfb5ca68c3e9016d15c87
Sai Annam <107242846+otaku0304@users.noreply.github.com>
2025-04-03 12:59:26 +0530

    feat: add site URL and internationalization URL to environment configuration and update components to use them

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,4 +3,6 @@ export const environment = {
   development: true,
   local: false,
   apiUrl: 'https://dev-pdf-flask.onrender.com',
+  siteUrl:'https://angular-pdf-pr-master.onrender.com',
+  navigateToi18n:'https://internationalization-d2aa6.web.app/en/#/home',
 };
