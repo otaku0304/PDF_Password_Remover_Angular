cea799d6c57bcbacaae87e527076add859315ccc
Sai Krishna Annam <deepakaanm143@gmail.com>
2025-04-09 12:45:20 +0530

    bump version to 1.1.0 and update sitemap URLs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,12 +16,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pdf-removal'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('pdf-removal');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
